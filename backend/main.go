e3c7696fc82081eaa403a90db656b22e3fc090e4
Sky Zhou <sky.zhou@indexexchange.com>
2019-09-17 12:35:43 -0400

    added handleConnection

--- backend/main.go
+++ backend/main.go
@@ -4,22 +4,20 @@ import (
         "log"
         "net/http"
 		"encoding/json"
-		"log"
-		"math/rand"
+		// "log"
+		// "math/rand"
 
         "github.com/gorilla/websocket"
 )
 
 type Room struct {
-	memberSockets  []*websocket.Conn
+	members  []*Client
 }
-var roomAssignments = make(map[string]string)
 
 var rooms []*Room
 
 // Configure the upgrader
 var upgrader = websocket.Upgrader{}
-
 
 const (
 	TypeConnect = iota
@@ -47,13 +45,17 @@ type Receive struct {
 }
 
 type User struct {
-	DeviceId string
-	Username string
 }
 
 type Client struct {
-	socket *websocket.Conn
+	socket *websocket.Conn 
 	room *Room
+	DeviceId string 
+	Username string
+}
+
+type Response struct {
+	status int `json:"status"`
 }
 
 func main() {
@@ -72,15 +74,8 @@ func handleMessage(w http.ResponseWriter, r *http.Request) {
 	defer ws.Close()
 
 	for {
-
-		_, request, err := ws.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			return
-		}
-
 		var message Message
-		err = json.Unmarshal(request, &message)
+		err = ws.ReadJSON(&message)
 		if err != nil {
 			log.Printf("Error unmarshalling message: %s", err)
 			continue
@@ -89,8 +84,12 @@ func handleMessage(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Parsed message: %s", message)
 
 		if message.Type == TypeConnect {
+			client := Client{
+				socket: ws,
+				
+			}
 
-			handleConnect(message.Data)
+			client.handleConnect(message.Data)
 
 		} else if message.Type == TypeSend {
 
@@ -99,8 +98,9 @@ func handleMessage(w http.ResponseWriter, r *http.Request) {
 
 		}
 
-
+		websocket.WriteJSON(ws, Response{status: 0})
 	}
+
 
 }
 
@@ -112,57 +112,61 @@ func (client *Client) handleConnect(data json.RawMessage) {
 		return
 	}
 
-	numRooms := len(rooms)
-	if numRooms == 0 {
-		rooms = append(rooms, &Room{
-			participants: []*websocket.Conn{client.socket},
-		})
-	}
-	for i, room := range rooms {
-		if (len(room.participants) < maxGroupSize) { //TODO race condition here lul
-			room.memberSocket = append(room.memberSocket, client.socket)
-		}
-	} 
-	connect.DeviceId
-
-}
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	// Upgrade initial GET request to a websocket
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Make sure we close the connection when the function returns
-	defer ws.Close()
-
-	// Register our new client
-	clients[ws] = true
-	for {
-		var msg Message
-		// Read in a new message as JSON and map it to a Message object
-		err := ws.ReadJSON(&msg)
-		if err != nil {
-			log.Printf("error: %v", err)
-			delete(clients, ws)
+	
+	for _, room := range rooms {
+		if (len(room.members) < maxGroupSize) { //TODO race condition here lul
+			room.members = append(room.members, client)
+			client.room = room
 			break
 		}
-		// Send the newly received message to the broadcast channel
-		broadcast <- msg
-	}
-}
+	} 
 
-func handleMessages() {
-	for {
-		// Grab the next message from the broadcast channel
-		msg := <-broadcast
-		// Send it out to every client that is currently connected
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				log.Printf("error: %v", err)
-				client.Close()
-				delete(clients, client)
-			}
+	if client.room == nil {
+		newRoom := &Room{
+			members: []*Client{client},
 		}
+		rooms = append(rooms, newRoom)
+		client.room = newRoom
 	}
+	
 }
+// func handleConnections(w http.ResponseWriter, r *http.Request) {
+// 	// Upgrade initial GET request to a websocket
+// 	ws, err := upgrader.Upgrade(w, r, nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	// Make sure we close the connection when the function returns
+// 	defer ws.Close()
+
+// 	// Register our new client
+// 	clients[ws] = true
+// 	for {
+// 		var msg Message
+// 		// Read in a new message as JSON and map it to a Message object
+// 		err := ws.ReadJSON(&msg)
+// 		if err != nil {
+// 			log.Printf("error: %v", err)
+// 			delete(clients, ws)
+// 			break
+// 		}
+// 		// Send the newly received message to the broadcast channel
+// 		broadcast <- msg
+// 	}
+// }
+
+// func handleMessages() {
+// 	for {
+// 		// Grab the next message from the broadcast channel
+// 		msg := <-broadcast
+// 		// Send it out to every client that is currently connected
+// 		for client := range clients {
+// 			err := client.WriteJSON(msg)
+// 			if err != nil {
+// 				log.Printf("error: %v", err)
+// 				client.Close()
+// 				delete(clients, client)
+// 			}
+// 		}
+// 	}
+// }
