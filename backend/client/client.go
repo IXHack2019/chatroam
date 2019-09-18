5db64699a275a9a898b603de5e20f20c2abcbc3c
adam.wong <adam.wong@indexexchange.com>
2019-09-18 01:01:39 -0400

    Fix client log formatting

--- backend/client/client.go
+++ backend/client/client.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"net/url"
 	"os"
 	"strings"
@@ -12,14 +11,12 @@ import (
 )
 
 func main() {
-	log.SetFlags(log.LstdFlags)
-
 	u := url.URL{Scheme: "ws", Host: "localhost:8888", Path: "/connect"}
-	log.Printf("connecting to %s", u.String())
+	fmt.Printf("connecting to %s\n", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Fatal("dial:", err)
+		fmt.Printf("dial: %s", err)
 	}
 	defer c.Close()
 
@@ -29,30 +26,30 @@ func main() {
 	deviceID = strings.Replace(deviceID, "\n", "", -1)
 
 	connectMsg := fmt.Sprintf(`{"type": 0, "data": {"deviceId": "%s", "lon": -79.380688, "lat":43.652112 } }`, deviceID)
-	log.Printf("Sending message: %s\n", connectMsg)
+	fmt.Printf("Sending message: %s\n", connectMsg)
 
 	err = c.WriteMessage(websocket.TextMessage, []byte(connectMsg))
 	if err != nil {
-		log.Println("write:", err)
+		fmt.Println("write:", err)
 		return
 	}
 
 	_, message, err := c.ReadMessage()
 	if err != nil {
-		log.Println("read:", err)
+		fmt.Println("read:", err)
 		return
 	}
-	log.Printf("Connect response: %s\n", message)
+	fmt.Printf("Connect response: %s\n", message)
 
 	go func() {
 		for {
 			_, message, err := c.ReadMessage()
 			if err != nil {
-				log.Println("read:", err)
+				fmt.Println("read:", err)
 				return
 			}
 
-			log.Printf("\nReceived message: %sEnter JSON to send: ", message)
+			fmt.Printf("\nReceived message: %s\nEnter JSON to send: ", message)
 		}
 	}()
 
@@ -60,11 +57,11 @@ func main() {
 		fmt.Print("Enter JSON to send: ")
 		json, _ := reader.ReadString('\n')
 		json = strings.Replace(json, "\n", "", -1)
-		log.Printf("Sending message: %s\n", json)
+		fmt.Printf("Sending message: %s\n", json)
 
 		err = c.WriteMessage(websocket.TextMessage, []byte(json))
 		if err != nil {
-			log.Println("write:", err)
+			fmt.Println("write:", err)
 			return
 		}
 	}
